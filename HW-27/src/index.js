8a9afb99b8a868a5bed1e30c63427d3feffa3fae
Nataliia Kubytska <natashakub27@gmail.com>
2023-03-09 10:09:16 +0200

    Implement hw 26-28

--- HW-27/src/index.js
+++ HW-27/src/index.js
@@ -1,1 +1,20 @@
 // Вивести таблицю 10 × 10, заповнену числами від 1 до 100 (таблиця створюється динамічно)
+const table = document.createElement("table");
+table.classList.add("table");
+
+for (let i = 0; i < 10; i++) {
+  const row = document.createElement("tr");
+
+  for (let j = 0; j < 10; j++) {
+    const cellValue = i * 10 + j + 1;
+
+    const cell = document.createElement("td");
+    cell.textContent = cellValue;
+
+    row.append(cell);
+  }
+
+  table.append(row);
+}
+
+document.body.append(table);
